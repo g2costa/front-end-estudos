57ff9fb46e4b132999ebfbd827857159656e0a3d
g2costa <guilherme.di.aquino@hotmail.com>
2024-12-10 13:11:17 -0300

    Contador

--- HTML-CSS-JS/5-contador/script.js
+++ HTML-CSS-JS/5-contador/script.js
@@ -1,19 +1,12 @@
-const boxDiv = document.getElementById("box")
+
+const numero = document.getElementById("numero")
+const boxContainer = document.getElementById("box")
+
+boxContainer.addEventListener("click", incrementarNumero)
 
 
-boxDiv.addEventListener("click", trocarCor)
-
-
-var colorDivDefault = 1
-
-function trocarCor(){
-    if(colorDivDefault == 1){
-        boxDiv.classList.remove("style-1")
-        boxDiv.classList.add("style-2")
-        colorDivDefault = 0
-    }else{
-        boxDiv.classList.remove("style-2")
-        boxDiv.classList.add("style-1")
-        colorDivDefault = 1
-    }
+var contador = 1
+function incrementarNumero(){
+    numero.innerText = contador
+    contador += 1
 }
